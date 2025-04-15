c3d4f80cb173d5822dac8b91a4df00a46f7d1934
Ayushi-Jaddhari <jaddharia@gmail.com>
2025-04-15 15:29:27 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,40 @@
+/** Nested parent-child
+ *
+ * <div id=" parent">
+ *      <div id= "child">
+ *           <h1></h1>
+ *      </div>
+ * </div>
+ *
+ *
+ *
+ */
+/**
+ * SIblings
+ * <h1>I am h1 tag</h1>
+ * <h2> I am h2 tag</h2>
+ * 
+ * 
+ * We will use array for writing child
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },[
+    React.createElement("h1", {}, "I am h1 tag!"),
+    React.createElement("h2", {}, "I am h2 tag!"),
+
+    ]
+  )
+);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React!"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
